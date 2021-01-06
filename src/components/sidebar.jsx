590784c9b5349c892fa4f0912134293afe059f98
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-01-06 21:26:35 +0530

    Update the instagram link

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -27,7 +27,7 @@ export default class Sidebar extends Component {
               <ul>
                 <li><a href="https://www.facebook.com/aniruddha2k/" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
                 <li><a href="https://twitter.com/aniruddha_2000/" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.instagram.com/__gitignore__/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+                <li><a href="https://www.instagram.com/_aniruddha_basak_/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
                 <li><a href="https://www.linkedin.com/in/aniruddha2000/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/aniruddha2000/" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
                 <li><a href="https://aniruddhab.home.blog/" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
