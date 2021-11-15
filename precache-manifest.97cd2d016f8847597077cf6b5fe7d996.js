72f13d9939b846673fd1ea3a792060910d50bd0d
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-11-15 22:12:20 +0530

    Updates

---
+++ precache-manifest.97cd2d016f8847597077cf6b5fe7d996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28f44578e3b6292b953989db2d84e9a",
+    "url": "/aniruddhabasak/index.html"
+  },
+  {
+    "revision": "d19a1455da670a29eda8",
+    "url": "/aniruddhabasak/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "6a4644923c66e30d2dc0",
+    "url": "/aniruddhabasak/static/js/2.1a20d560.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/aniruddhabasak/static/js/2.1a20d560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19a1455da670a29eda8",
+    "url": "/aniruddhabasak/static/js/main.a63cb2ac.chunk.js"
+  },
+  {
+    "revision": "0ee8e3edc4a717ceecf8",
+    "url": "/aniruddhabasak/static/js/runtime-main.bb043161.js"
+  }
+]);
