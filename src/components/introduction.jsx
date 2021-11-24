4c5d35b75cd6e67ac146899278bdc47114cfa2a0
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-11-24 11:12:42 +0530

    Update the resume

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -25,7 +25,7 @@ export default class Introduction extends Component {
                           <p>
                             <a
                               className="btn btn-primary btn-learn"
-                              href="https://drive.google.com/file/d/11qLyjXxpwmLvCZSulYUhOgQ5KGvKa1EX/view?usp=sharing"
+                              href="https://drive.google.com/file/d/1GcegakTgteatmOUsyzaBgcUyJr8_q4xR/view?usp=sharing"
                               target="_blank"
                               rel="noopener noreferrer"
                             >
