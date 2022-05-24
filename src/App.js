942efcd96842836778d05af7467069151c484133
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-05-24 21:59:13 +0530

    Add resume and update componenet
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import Sidebar from "./components/sidebar";
 import Introduction from "./components/introduction";
 import About from "./components/about";
 import Timeline from "./components/timeline";
+import Resume from "./components/resume";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div id="colorlib-page">
-        <div id="container-wrap">
-          <Sidebar></Sidebar>
-          <div id="colorlib-main">
-            <Introduction></Introduction>
-            <About></About>
-            <Timeline></Timeline>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/">
+            <div id="colorlib-page">
+              <div id="container-wrap">
+                <Sidebar></Sidebar>
+                <div id="colorlib-main">
+                  <Introduction></Introduction>
+                  <About></About>
+                  <Timeline></Timeline>
+                </div>
+              </div>
+            </div>
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+        </Routes>
+      </Router>
     );
   }
 }
---
+++ src/components/resume.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <embed
+          src="../../resume/Aniruddha_Basak_cv.pdf"
+          type="application/pdf"
+          width="100%"
+          height="850px"
+        />
+      </div>
+    );
+  }
+}
